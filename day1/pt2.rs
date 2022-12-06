fbcfef89fd269d444ec23c620128cbc2f95bdd75
Jacob Duncan <jacobduncan00@yahoo.com>
2022-12-05 21:50:57 -0500

    add pt2

---
+++ day1/pt2.rs
@@ -0,0 +1,28 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(lines) = read_lines("./input.txt") {
+        let mut calorie_vec: Vec<i32> = vec![];
+        let mut totals_vec: Vec<i32> = vec![];
+        for line in lines {
+            if let Ok(calorie) = line {
+                if calorie.is_empty() {
+                    totals_vec.push(calorie_vec.iter().sum());
+                    calorie_vec.clear();
+                } else {
+                    calorie_vec.push(calorie.parse().unwrap());
+                }
+            }
+        }
+        totals_vec.sort_by(|a, b| b.cmp(a));
+        println!("3 Highest Calories Sum: {}", totals_vec[0] + totals_vec[1] + totals_vec[2]);
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
