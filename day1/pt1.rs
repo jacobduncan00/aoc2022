ac8be6b39598e3a5e457aba92a8b2d02cd9e8e67
Jacob Duncan <jacobduncan00@yahoo.com>
2022-12-06 15:09:27 -0500

    refactor

--- day1/pt1.rs
+++ day1/pt1.rs
@@ -4,15 +4,15 @@ use std::path::Path;
 
 fn main() {
     if let Ok(lines) = read_lines("./input.txt") {
-        let mut calorie_vec: Vec<i32> = vec![];
+        let mut calorie_sum: i32 = 0;
         let mut totals_vec: Vec<i32> = vec![];
         for line in lines {
             if let Ok(calorie) = line {
                 if calorie.is_empty() {
-                    totals_vec.push(calorie_vec.iter().sum());
-                    calorie_vec.clear();
+                    totals_vec.push(calorie_sum);
+                    calorie_sum = 0;
                 } else {
-                    calorie_vec.push(calorie.parse().unwrap());
+                    calorie_sum += calorie.parse::<i32>().unwrap();
                 }
             }
         }
@@ -21,7 +21,9 @@ fn main() {
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
--- day1/pt2.rs
+++ day1/pt2.rs
@@ -4,25 +4,30 @@ use std::path::Path;
 
 fn main() {
     if let Ok(lines) = read_lines("./input.txt") {
-        let mut calorie_vec: Vec<i32> = vec![];
+        let mut calorie_sum: i32 = 0;
         let mut totals_vec: Vec<i32> = vec![];
         for line in lines {
             if let Ok(calorie) = line {
                 if calorie.is_empty() {
-                    totals_vec.push(calorie_vec.iter().sum());
-                    calorie_vec.clear();
+                    totals_vec.push(calorie_sum);
+                    calorie_sum = 0;
                 } else {
-                    calorie_vec.push(calorie.parse().unwrap());
+                    calorie_sum += calorie.parse::<i32>().unwrap();
                 }
             }
         }
         totals_vec.sort_by(|a, b| b.cmp(a));
-        println!("3 Highest Calories Sum: {}", totals_vec[0] + totals_vec[1] + totals_vec[2]);
+        println!(
+            "3 Highest Calories Sum: {}",
+            totals_vec[0] + totals_vec[1] + totals_vec[2]
+        );
     }
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
